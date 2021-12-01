a8a8ae4bb43eb54447b7daf4cf9e933aff81d865
William Kerley <wjmkerley@gmail.com>
2021-12-01 17:38:34 +0000

    day 1 part 1 complete

---
+++ 1_1_sonar_sweep.py
@@ -0,0 +1,15 @@
+data = open("1_data.txt", "r")
+
+measurements = []
+
+for number in data:
+    measurements.append(int(number))
+
+increases = 0
+for i, measure in enumerate(measurements):
+    if measure > measurements[i-1]:
+        increases += 1
+    
+print(increases)
+
+data.close()
